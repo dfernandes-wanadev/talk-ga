e41d2d1baae8cb236d615a0cfd4a3affa393f2b7
Damien Fernandes <damien.fernandes24@gmail.com>
2024-11-16 15:04:54 +0100

    use slidev 0.49 and intro done

--- layouts/intro.vue
+++ layouts/intro.vue
@@ -1,11 +1,13 @@
 <template>
     <default class="intro">
+        <img v-if="eventImageSrc" :src="eventImageSrc" class="rounded-full p-2 absolute top-5 right-5" height="125"
+            width="125" />
         <div class="flex items-center">
             <figure class="w-1/2 px-12 relative">
                 <img :src="imageSrc" height="312" width="312" class="rounded-full p-2 bg-gradient-to-r from-[#6441db]
     via-[#964be6] to-[#ff5dff] dark:(from-white to-purple-50)" />
 
-                <img src="/gha_logo.svg"
+                <img v-if="subjectImageSrc" :src="subjectImageSrc"
                     class="rounded-full p-2 absolute top-0 right-5 bg-gradient-to-r from-[#6441db] via-[#964be6] to-[#ff5dff] dark:(from-white to-purple-50)"
                     height="150" width="150" />
                 <!-- TODO : Add logo event -->
@@ -26,6 +28,16 @@ const props = defineProps({
     introImage: {
         type: String,
     },
+    subjectImage: {
+        type: String,
+        default: "",
+    },
+    eventImage: {
+        type: String,
+        default: "",
+    },
 });
 const imageSrc = computed(() => resolveAssetUrl(props.introImage));
+const subjectImageSrc = computed(() => resolveAssetUrl(props.subjectImage));
+const eventImageSrc = computed(() => resolveAssetUrl(props.eventImage));
 </script>
